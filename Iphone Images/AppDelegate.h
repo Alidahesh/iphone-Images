70b180b3a13e009c828ed6a45590967b288589a5
Ali <alidahesh@yahoo.com>
2016-11-21 17:13:31 -0800

    Initial Commit

---
+++ Iphone Images/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Iphone Images
+//
+//  Created by Ali Dahesh on 2016-11-21.
+//  Copyright © 2016 Ali Dahesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
